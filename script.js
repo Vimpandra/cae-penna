26f59b1bd704da8ce55ad17b0d24604beed22738
Vimpandra <victorimperatriz@gmail.com>
2023-04-06 22:19:19 -0300

    Removed global variables

--- script.js
+++ script.js
@@ -1,50 +1,48 @@
-const words = {
-    names: [
-        'Shaco',
-        'Master Yi',
-        'Wukong',
-        'Elise',
-    ],
-    colors: [
-        'verde',
-        'cor de maconha',
-        'esverdeado',
-        'safira',
-        'esmeralda',
-    ],
-    dislikes: [
-        'Akali',
-        'Yumi',
-        'Trump',
-        'Bolsonarista cuzao gopista',
-        'Bater o dedinho do pé na quina',
-    ],
-}
-
-const pallet = {
-    foreground: ['black', 'navy', 'magenta', 'white'],
-    background: ['gold', 'cyan', 'pink', 'teal'],
-
-    randomColor: function() {
-        return Math.floor(Math.random()*4)
-    }
-}
-
-const spans = document.querySelectorAll('span');
-
 (function() {
+    const words = {
+        names: [
+            'Shaco',
+            'Master Yi',
+            'Wukong',
+            'Elise',
+        ],
+        colors: [
+            'verde',
+            'cor de maconha',
+            'esverdeado',
+            'safira',
+            'esmeralda',
+        ],
+        dislikes: [
+            'Akali',
+            'Yumi',
+            'Trump',
+            'Bolsonarista cuzao gopista',
+            'Bater o dedinho do pé na quina',
+        ],
+    }
+    
+    const pallet = {
+        foreground: ['black', 'navy', 'magenta', 'white'],
+        background: ['gold', 'cyan', 'pink', 'teal'],
+    
+        randomColor: function() {
+            return Math.floor(Math.random()*4)
+        }
+    }
+    
+    const spans = document.querySelectorAll('span');
+
     spans[0].addEventListener('click', () => {
         randomize(spans[0], words.names)
     });
-
     spans[1].addEventListener('click', () => {
         randomize(spans[1], words.colors)
     });
-
     spans[2].addEventListener('click', () => {
         randomize(spans[2], words.dislikes)
     });
-
+    
     function randomize(span, arr) {
         let randomNum = Math.floor(Math.random()*arr.length);
         if (span.textContent === arr[randomNum]) {
