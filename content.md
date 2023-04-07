78173cc19ee8c3961af573d06ce713c63e66e9fb
Vimpandra <victorimperatriz@gmail.com>
2023-04-06 22:03:46 -0300

    Basic working prototype

---
+++ content.md
@@ -0,0 +1,16 @@
+Shaco
+Master Yi
+Wukong
+Elise
+
+Verde
+Cor de maconha
+Esverdeado
+Safira
+Esmeralda
+
+Akali
+Yumi
+Trump
+Bolsonarista cuzao gopista
+Bater o dedinho do pé na quina
---
+++ script.js
@@ -0,0 +1,59 @@
+const words = {
+    names: [
+        'Shaco',
+        'Master Yi',
+        'Wukong',
+        'Elise',
+    ],
+    colors: [
+        'verde',
+        'cor de maconha',
+        'esverdeado',
+        'safira',
+        'esmeralda',
+    ],
+    dislikes: [
+        'Akali',
+        'Yumi',
+        'Trump',
+        'Bolsonarista cuzao gopista',
+        'Bater o dedinho do pé na quina',
+    ],
+}
+
+const pallet = {
+    foreground: ['black', 'navy', 'magenta', 'white'],
+    background: ['gold', 'cyan', 'pink', 'teal'],
+
+    randomize: function() {
+        return Math.floor(Math.random()*4)
+    }
+}
+
+const spans = document.querySelectorAll('span');
+
+(function() {
+    spans[0].addEventListener('click', () => {
+        randomize(spans[0], words.names)
+    });
+
+    spans[1].addEventListener('click', () => {
+        randomize(spans[1], words.colors)
+    });
+
+    spans[2].addEventListener('click', () => {
+        randomize(spans[2], words.dislikes)
+    });
+
+    function randomize(span, arr) {
+        let randomNum = Math.floor(Math.random()*arr.length);
+        if (span.textContent === arr[randomNum]) {
+            randomize(span, arr);
+        } else {
+            span.textContent = arr[randomNum];
+            span.style.color = pallet.foreground[pallet.randomize()];
+            span.style.backgroundColor = pallet.background[pallet.randomize()];
+        }
+    };
+
+}());
